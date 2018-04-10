cf105dd5a4151cba3c6ec700b9c2f77b6aebc892
Miguel Rojas <miguel.rojas-ma@mail.escuelaing.edu.co>
2018-04-10 12:02:52 -0500

    configuracion inicial

---
+++ src/main/java/com/mycompany/treasuremining/TreasureMingApplication.java
@@ -0,0 +1,17 @@
+package com.mycompany.treasuremining;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class that runs the Spring Application for COOP Asteroids
+ * @author Miguel Rojas
+ */
+@SpringBootApplication
+public class TreasureMingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TreasureMingApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mycompany/treasuremining/model/Player.java
@@ -0,0 +1,43 @@
+package com.mycompany.treasuremining.model;
+
+/*
+ * @author Miguel Rojas
+ */
+public class Player {
+    private double playerX;
+    private double playerY;
+    private String playerId;
+    private int playerAngle;
+
+    public double getPlayerX() {
+        return playerX;
+    }
+
+    public void setPlayerX(double playerX) {
+        this.playerX = playerX;
+    }
+
+    public double getPlayerY() {
+        return playerY;
+    }
+
+    public void setPlayerY(double playerY) {
+        this.playerY = playerY;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(String playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getPlayerAngle() {
+        return playerAngle;
+    }
+
+    public void setPlayerAngle(int playerAngle) {
+        this.playerAngle = playerAngle;
+    }
+}
